0c0f351d404eb5d30b70ee3b30307cd53dbb1192
Matt Fleming <matthewfleming8@gmail.com>
2020-04-16 00:14:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,9 @@ class Anagram
   end
 
   def match(word_array)
-  word_array.select do |word|
-    word.split("").sort == @word.split("").sort
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
   end
+
 end
